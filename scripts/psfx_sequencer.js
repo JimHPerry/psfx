9884c86a84ea8143bad6b52109cea828470a3246
JimHPerry <perieskell@hotmail.com>
2024-11-02 22:56:04 +0000

    Learning what commit means

--- scripts/psfx_sequencer.js
+++ scripts/psfx_sequencer.js
@@ -1,33 +1,207 @@
 const psfxDatabase = {};
 export async function registerPSFXDatabase(prefix) {
 
-    psfxDatabase["toll-the-dead"] = {
-        "001": `${prefix}/library/toll-the-dead/toll-the-dead-001.ogg`,
-        "002": `${prefix}/library/toll-the-dead/toll-the-dead-002.ogg`,
-        "rasping-whispers": {
-            "001": `${prefix}/library/toll-the-dead/rasping-whispers-001.ogg`
+    psfxDatabase["weapon-swooshes"] = {
+        "light": {
+            "v1": {
+                "group01": [
+                    `${prefix}/library/weapon-swooshes/light/v1/group01/meleeattack-swoosh-light-group01-00.ogg`,
+                    `${prefix}/library/weapon-swooshes/light/v1/group01/meleeattack-swoosh-light-group01-01.ogg`,
+                    `${prefix}/library/weapon-swooshes/light/v1/group01/meleeattack-swoosh-light-group01-02.ogg`,
+                    `${prefix}/library/weapon-swooshes/light/v1/group01/meleeattack-swoosh-light-group01-03.ogg`
+                ],
+                "group02": [
+                    `${prefix}/library/weapon-swooshes/light/v1/group02/meleeattack-swoosh-light-group02-00.ogg`,
+                    `${prefix}/library/weapon-swooshes/light/v1/group02/meleeattack-swoosh-light-group02-01.ogg`,
+                    `${prefix}/library/weapon-swooshes/light/v1/group02/meleeattack-swoosh-light-group02-02.ogg`,
+                    `${prefix}/library/weapon-swooshes/light/v1/group02/meleeattack-swoosh-light-group02-03.ogg`
+                ],
+                "group03": [
+                    `${prefix}/library/weapon-swooshes/light/v1/group03/meleeattack-swoosh-light-group03-00.ogg`,
+                    `${prefix}/library/weapon-swooshes/light/v1/group03/meleeattack-swoosh-light-group03-01.ogg`,
+                    `${prefix}/library/weapon-swooshes/light/v1/group03/meleeattack-swoosh-light-group03-02.ogg`,
+                    `${prefix}/library/weapon-swooshes/light/v1/group03/meleeattack-swoosh-light-group03-03.ogg`
+                ],
+                "group04": [
+                    `${prefix}/library/weapon-swooshes/light/v1/group04/meleeattack-swoosh-light-group04-00.ogg`,
+                    `${prefix}/library/weapon-swooshes/light/v1/group04/meleeattack-swoosh-light-group04-01.ogg`,
+                    `${prefix}/library/weapon-swooshes/light/v1/group04/meleeattack-swoosh-light-group04-02.ogg`,
+                    `${prefix}/library/weapon-swooshes/light/v1/group04/meleeattack-swoosh-light-group04-03.ogg`
+                ],
+                "group05": [
+                    `${prefix}/library/weapon-swooshes/light/v1/group05/meleeattack-swoosh-light-group05-00.ogg`,
+                    `${prefix}/library/weapon-swooshes/light/v1/group05/meleeattack-swoosh-light-group05-01.ogg`,
+                    `${prefix}/library/weapon-swooshes/light/v1/group05/meleeattack-swoosh-light-group05-02.ogg`,
+                    `${prefix}/library/weapon-swooshes/light/v1/group05/meleeattack-swoosh-light-group05-03.ogg`
+                ],
+                "group06": [
+                    `${prefix}/library/weapon-swooshes/light/v1/group06/meleeattack-swoosh-light-group06-00.ogg`,
+                    `${prefix}/library/weapon-swooshes/light/v1/group06/meleeattack-swoosh-light-group06-01.ogg`
+                ]
+            }
         },
-        "ominous-bell": {
-            "001": `${prefix}/library/toll-the-dead/ominous-bell-001.ogg`
+        "heavy": {
+            "v1": {
+                "group01": [
+                    `${prefix}/library/weapon-swooshes/heavy/v1/group01/meleeattack-swoosh-heavy-group01-00.ogg`,
+                    `${prefix}/library/weapon-swooshes/heavy/v1/group01/meleeattack-swoosh-heavy-group01-01.ogg`,
+                    `${prefix}/library/weapon-swooshes/heavy/v1/group01/meleeattack-swoosh-heavy-group01-02.ogg`,
+                    `${prefix}/library/weapon-swooshes/heavy/v1/group01/meleeattack-swoosh-heavy-group01-03.ogg`
+                ],
+                "group02": [
+                    `${prefix}/library/weapon-swooshes/heavy/v1/group02/meleeattack-swoosh-heavy-group02-00.ogg`,
+                    `${prefix}/library/weapon-swooshes/heavy/v1/group02/meleeattack-swoosh-heavy-group02-01.ogg`,
+                    `${prefix}/library/weapon-swooshes/heavy/v1/group02/meleeattack-swoosh-heavy-group02-02.ogg`,
+                    `${prefix}/library/weapon-swooshes/heavy/v1/group02/meleeattack-swoosh-heavy-group02-03.ogg`
+                ],
+                "group03": [
+                    `${prefix}/library/weapon-swooshes/heavy/v1/group03/meleeattack-swoosh-heavy-group03-00.ogg`,
+                    `${prefix}/library/weapon-swooshes/heavy/v1/group03/meleeattack-swoosh-heavy-group03-01.ogg`,
+                    `${prefix}/library/weapon-swooshes/heavy/v1/group03/meleeattack-swoosh-heavy-group03-02.ogg`,
+                    `${prefix}/library/weapon-swooshes/heavy/v1/group03/meleeattack-swoosh-heavy-group03-03.ogg`
+                ],
+                "group04": [
+                    `${prefix}/library/weapon-swooshes/heavy/v1/group04/meleeattack-swoosh-heavy-group04-00.ogg`,
+                    `${prefix}/library/weapon-swooshes/heavy/v1/group04/meleeattack-swoosh-heavy-group04-01.ogg`,
+                    `${prefix}/library/weapon-swooshes/heavy/v1/group04/meleeattack-swoosh-heavy-group04-02.ogg`,
+                    `${prefix}/library/weapon-swooshes/heavy/v1/group04/meleeattack-swoosh-heavy-group04-03.ogg`
+                ],
+                "group05": [
+                    `${prefix}/library/weapon-swooshes/heavy/v1/group05/meleeattack-swoosh-heavy-group05-00.ogg`,
+                    `${prefix}/library/weapon-swooshes/heavy/v1/group05/meleeattack-swoosh-heavy-group05-01.ogg`,
+                    `${prefix}/library/weapon-swooshes/heavy/v1/group05/meleeattack-swoosh-heavy-group05-02.ogg`,
+                    `${prefix}/library/weapon-swooshes/heavy/v1/group05/meleeattack-swoosh-heavy-group05-03.ogg`
+                ],
+                "group06": [
+                    `${prefix}/library/weapon-swooshes/heavy/v1/group06/meleeattack-swoosh-heavy-group06-00.ogg`,
+                    `${prefix}/library/weapon-swooshes/heavy/v1/group06/meleeattack-swoosh-heavy-group06-01.ogg`
+                ]
+            }
+        },
+        "fire": {
+            "v1": {
+                "group01": [
+                    `${prefix}/library/weapon-swooshes/fire/v1/group01/meleeattack-swoosh-magicaleffect-group01-fire-00.ogg`,
+                    `${prefix}/library/weapon-swooshes/fire/v1/group01/meleeattack-swoosh-magicaleffect-group01-fire-01.ogg`,
+                    `${prefix}/library/weapon-swooshes/fire/v1/group01/meleeattack-swoosh-magicaleffect-group01-fire-02.ogg`,
+                    `${prefix}/library/weapon-swooshes/fire/v1/group01/meleeattack-swoosh-magicaleffect-group01-fire-03.ogg`
+                ]
+            }
+        },
+        "cold": {
+            "v1": {
+                "group01": [
+                    `${prefix}/library/weapon-swooshes/cold/v1/group01/meleeattack-swoosh-magicaleffect-group01-cold-00.ogg`,
+                    `${prefix}/library/weapon-swooshes/cold/v1/group01/meleeattack-swoosh-magicaleffect-group01-cold-01.ogg`,
+                    `${prefix}/library/weapon-swooshes/cold/v1/group01/meleeattack-swoosh-magicaleffect-group01-cold-02.ogg`,
+                    `${prefix}/library/weapon-swooshes/cold/v1/group01/meleeattack-swoosh-magicaleffect-group01-cold-03.ogg`
+                ]
+            }
+        },
+        "necrotic": {
+            "v1": {
+                "group01": [
+                    `${prefix}/library/weapon-swooshes/necrotic/v1/group01/meleeattack-swoosh-magicaleffect-group01-necrotic-00.ogg`,
+                    `${prefix}/library/weapon-swooshes/necrotic/v1/group01/meleeattack-swoosh-magicaleffect-group01-necrotic-01.ogg`,
+                    `${prefix}/library/weapon-swooshes/necrotic/v1/group01/meleeattack-swoosh-magicaleffect-group01-necrotic-02.ogg`,
+                    `${prefix}/library/weapon-swooshes/necrotic/v1/group01/meleeattack-swoosh-magicaleffect-group01-necrotic-03.ogg`
+                ]
+            }
+        },
+        "psychic": {
+            "v1": {
+                "group01": [
+                    `${prefix}/library/weapon-swooshes/psychic/v1/group01/meleeattack-swoosh-magicaleffect-group01-psychic-00.ogg`,
+                    `${prefix}/library/weapon-swooshes/psychic/v1/group01/meleeattack-swoosh-magicaleffect-group01-psychic-01.ogg`,
+                    `${prefix}/library/weapon-swooshes/psychic/v1/group01/meleeattack-swoosh-magicaleffect-group01-psychic-02.ogg`,
+                    `${prefix}/library/weapon-swooshes/psychic/v1/group01/meleeattack-swoosh-magicaleffect-group01-psychic-03.ogg`
+                ]
+            }
         }
     }
-    psfxDatabase["melee-attack"] = {
-        "swoosh": {
-            "001": [
-                `${prefix}/library/melee-attack/swoosh-01/melee-attack-swoosh-01-01.ogg`,
-                `${prefix}/library/melee-attack/swoosh-01/melee-attack-swoosh-01-02.ogg`,
-                `${prefix}/library/melee-attack/swoosh-01/melee-attack-swoosh-01-03.ogg`,
-                `${prefix}/library/melee-attack/swoosh-01/melee-attack-swoosh-01-04.ogg`
+    psfxDatabase["impacts"] = {
+        "slashing": {
+            "v1": [
+                `${prefix}/library/impacts/slashing/v1/meleeattack-impacts-slashing-00.ogg`,
+                `${prefix}/library/impacts/slashing/v1/meleeattack-impacts-slashing-01.ogg`,
+                `${prefix}/library/impacts/slashing/v1/meleeattack-impacts-slashing-02.ogg`,
+                `${prefix}/library/impacts/slashing/v1/meleeattack-impacts-slashing-03.ogg`  
+            ]    
+        },
+        "bludgeoning": {
+            "v1": [
+                `${prefix}/library/impacts/bludgeoning/v1/meleeattack-impacts-bludgeoning-00.ogg`,
+                `${prefix}/library/impacts/bludgeoning/v1/meleeattack-impacts-bludgeoning-01.ogg`,
+                `${prefix}/library/impacts/bludgeoning/v1/meleeattack-impacts-bludgeoning-02.ogg`,
+                `${prefix}/library/impacts/bludgeoning/v1/meleeattack-impacts-bludgeoning-03.ogg`  
+            ]    
+        },
+        "magicaleffects": {
+            "fire": [
+                `${prefix}/library/impacts/magicaleffects/fire/meleeattack-impacts-magicaleffects-fire-00.ogg` 
+            ],
+             "cold": [
+                `${prefix}/library/impacts/magicaleffects/cold/meleeattack-impacts-magicaleffects-cold-00.ogg` 
+            ],
+            "necrotic": [
+                `${prefix}/library/impacts/magicaleffects/necrotic/meleeattack-impacts-magicaleffects-necrotic-00.ogg` 
+            ],
+            "psychic": [
+                `${prefix}/library/impacts/magicaleffects/psychic/meleeattack-impacts-magicaleffects-psychic-00.ogg`,
+                `${prefix}/library/impacts/magicaleffects/psychic/meleeattack-impacts-magicaleffects-psychic-01.ogg`,
+                `${prefix}/library/impacts/magicaleffects/psychic/meleeattack-impacts-magicaleffects-psychic-02.ogg`,
+                `${prefix}/library/impacts/magicaleffects/psychic/meleeattack-impacts-magicaleffects-psychic-03.ogg` 
+            ]   
+        }
+    }
+    psfxDatabase["cantrips"] = {
+        "fire-bolt": {
+            "v1": {
+                "05ft": `${prefix}/library/cantrips/fire-bolt/v1/fire-bolt-001-05ft.ogg`,
+                "15ft": `${prefix}/library/cantrips/fire-bolt/v1/fire-bolt-001-15ft.ogg`,
+                "30ft": `${prefix}/library/cantrips/fire-bolt/v1/fire-bolt-001-30ft.ogg`,
+                "60ft": `${prefix}/library/cantrips/fire-bolt/v1/fire-bolt-001-60ft.ogg`,
+                "90ft": `${prefix}/library/cantrips/fire-bolt/v1/fire-bolt-001-90ft.ogg`
+                },
+            "v2": {
+                "05ft": `${prefix}/library/cantrips/fire-bolt/v2/fire-bolt-002-05ft.ogg`,
+                "15ft": `${prefix}/library/cantrips/fire-bolt/v2/fire-bolt-002-15ft.ogg`,
+                "30ft": `${prefix}/library/cantrips/fire-bolt/v2/fire-bolt-002-30ft.ogg`,
+                "60ft": `${prefix}/library/cantrips/fire-bolt/v2/fire-bolt-002-60ft.ogg`,
+                "90ft": `${prefix}/library/cantrips/fire-bolt/v2/fire-bolt-002-90ft.ogg`
+                }
+        },
+        "ray-of-frost": {
+            "v1": [
+                `${prefix}/library/cantrips/ray-of-frost/v1/ray-of-frost-001.ogg`,
             ]
-        }
-    }
-    psfxDatabase["fire-bolt"] = {
-        "001": {
-            "05ft": `${prefix}/library/fire-bolt/fire-bolt-001-05ft.ogg`,
-            "15ft": `${prefix}/library/fire-bolt/fire-bolt-001-15ft.ogg`,
-            "30ft": `${prefix}/library/fire-bolt/fire-bolt-001-30ft.ogg`,
-            "60ft": `${prefix}/library/fire-bolt/fire-bolt-001-60ft.ogg`,
-            "90ft": `${prefix}/library/fire-bolt/fire-bolt-001-90ft.ogg`
+        },
+        "toll-the-dead": {
+            "v1": {
+                "001": `${prefix}/library/cantrips/toll-the-dead/v1/toll-the-dead-001.ogg`,
+                "002": `${prefix}/library/cantrips/toll-the-dead/v1/toll-the-dead-002.ogg`,
+            },
+            "rasping-whispers": {
+                "001": `${prefix}/library/cantrips/toll-the-dead/v1/rasping-whispers-001.ogg`
+                },
+            "ominous-bell": {
+                "001": `${prefix}/library/cantrips/toll-the-dead/v1/ominous-bell-001.ogg`
+                }
+            
+        },
+        "mind-sliver": {
+            "v1": [
+                `${prefix}/library/cantrips/mind-sliver/v1/mind-sliver-001.ogg`,
+            ]
+        },
+        "acid-splash": {
+            "v1": {
+                "05ft": `${prefix}/library/cantrips/acid-splash/v1/acid-splash-001-05ft.ogg`,
+                "15ft": `${prefix}/library/cantrips/acid-splash/v1/acid-splash-001-15ft.ogg`,
+                "30ft": `${prefix}/library/cantrips/acid-splash/v1/acid-splash-001-30ft.ogg`,
+                "60ft": `${prefix}/library/cantrips/acid-splash/v1/acid-splash-001-60ft.ogg`,
+                "90ft": `${prefix}/library/cantrips/acid-splash/v1/acid-splash-001-90ft.ogg`
+                }
         }
     }
 }
